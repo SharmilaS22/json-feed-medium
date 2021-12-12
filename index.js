07ea0f1216b6ba20dd2c811cb02e2cfe26323e68
SharmilaS22 <sharmilasureshkumar22@gmail.com>
2021-12-12 14:23:41 +0530

    recent posts feature added

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const USERNAME = '@sharmilas';  //username to retrieve posts
 
 const MEDIUM_URL = `https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@${USERNAME}`;
 
+
+app.get('/', (req, res) => {
+    res.send({
+        feed: "/posts",
+        recentPosts: "/recent-posts"
+    })
+})
+
 app.get("/posts", (req, res, next) => {
   request.get(MEDIUM_URL, (err, apiResponse, body) => {
     if (!err && apiResponse.statusCode === 200) {
@@ -19,4 +27,25 @@ app.get("/posts", (req, res, next) => {
   });
 });
 
+app.get("/recent-posts", (req, res) => {
+    request.get(MEDIUM_URL, (err, apiResponse, body) => {
+        if (!err && apiResponse.statusCode === 200) {
+            data = JSON.parse(body)
+            
+            const posts = [];
+            data.items.forEach(post => {
+                posts.push({ 
+                    title: post.title,
+                    link: post.link
+                })
+            });
+
+            res.send({
+                postsArray: posts
+            })
+        } else {
+          res.status(500).send(err);
+        }
+      });
+})
 app.listen(PORT, () => console.log("Server started at " + PORT));
